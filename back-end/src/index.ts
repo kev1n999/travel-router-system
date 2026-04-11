0ab5e3e0b284d437b5f9235fc645bcdd8fd132e0
kev1n999 <sopratesta999@gmail.com>
2026-04-11 12:49:56 -0300

    feat: add function to validate env variables

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,11 +1,9 @@
 import "dotenv/config";
-import { env } from "./config/env";
 import { app } from "./server";
 import { mongoConnect } from "./database";
+import { validateEnv } from "./utils/env.validate";
 
-const { serverPort, mongoUri } = env;
-if (!serverPort) throw new Error("An error ocurred! You need to add the SERVER_PORT in .env!");
-if (!mongoUri) throw new Error("An error ocurred! You need to add the mongoURI in .env!");
+const { serverPort, mongoUri } = validateEnv();
 
 async function bootstrap() {
   await mongoConnect(mongoUri);
