3f359738576d7bb3f426072950cad4630cdd9b89
kev1n999 <sopratesta999@gmail.com>
2026-04-12 23:58:46 -0300

    feat: add the first server request to create a travel

--- front-end/src/components/create-travel-form.tsx
+++ front-end/src/components/create-travel-form.tsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
+import { createTravel } from "../services/create-travel";
+import CreateTravelButton from "./create-travel";
+
 export default function CreateTravelForm({ onClose }: { onClose: () => void; }) {
+  const [name, setName] = useState<string>("");
+  const createNewTravel = async (): Promise<void> => {
+    if (!name) return;
+    const travelId = await createTravel(name);
+    localStorage.setItem("travelId", travelId);
+    onClose();
+  };
+
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="absolute inset-0 bg-black/50" onClick={onClose} />
       <div className="relative bg-white p-6 rounded-xl shadow-lg">
-        <form>
+        <form onSubmit={(e) => e.preventDefault()}>
           <h1 className="text-4xl text-white mb-4">Criar viagem</h1>
           <label className="text-blue-500 block mb-1">
             Digite o nome da viagem
           </label>
-          <input className="w-full border rounded-sm outline-none" />
-          <button className="rounded-sm bg-blue-500 p-1 relative mt-4 cursor-pointer">
-            Criar Viagem
-          </button>
+          <input onChange={(i) => setName(i.target.value)} className="w-full border rounded-sm outline-none" />
+          <CreateTravelButton onClick={createNewTravel} />
         </form>
       </div>
     </div>
--- front-end/src/components/create-travel.tsx
+++ front-end/src/components/create-travel.tsx
@@ -1,4 +1,4 @@
-export default function CreateTravelButtton({ onClick }: { onClick: () => void; }) {
+export default function CreateTravelButton({ onClick }: { onClick: () => void; }) {
   return (
     <button onClick={onClick} className="bg-blue-500 transition-colors duration-300 hover:bg-blue-600 rounded-sm p-0.5 cursor-pointer">
       Criar Viagem
