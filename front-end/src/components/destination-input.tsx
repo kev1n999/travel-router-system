91c0de9e4afa568ccdefbd244582de9c31f4be01
kev1n999 <sopratesta999@gmail.com>
2026-04-13 11:17:33 -0300

    change buttons color

--- front-end/src/components/destination-input.tsx
+++ front-end/src/components/destination-input.tsx
@@ -13,7 +13,7 @@ export default function DestinationInput({ value, onChange, onSearch }: Props) {
         value={value}
         onChange={(e) => onChange(e.target.value)}
         onKeyDown={(e) => { if (e.key === "Enter") onSearch(); }}
-        className="outline-none rounded-sm bg-blue-200 p-1 font-normal placeholder:text-sm"
+        className="outline-none rounded-sm bg-white border border-neutral-400 p-1 font-normal placeholder:text-sm"
         placeholder="Buscar um destino específico..."
       />
       <SearchButton onSearch={onSearch} />
--- front-end/src/components/search-button.tsx
+++ front-end/src/components/search-button.tsx
@@ -4,6 +4,6 @@ interface SearchButtonProps {
 
 export default function SearchButton({ onSearch }: SearchButtonProps) {
   return (
-    <button onClick={onSearch} className="bg-blue-500 transition-colors duration-300 hover:bg-blue-600 rounded-sm p-0.5 cursor-pointer">Buscar</button>
+    <button onClick={onSearch} className="text-white bg-blue-500 transition-colors duration-300 hover:bg-blue-600 rounded-sm p-0.5 cursor-pointer">Buscar</button>
   )
 }
